0a17309807085c124e6d324dbd36ad11fe473c8d
Braulio Sandi <36654323+brav88@users.noreply.github.com>
2022-09-28 20:46:35 -0600

    Add files via upload

---
+++ src/java/webapp/products/Product.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package webapp.products;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Product {
+    String Name;
+    Integer Price;
+    
+    public Product(String pName, Integer pPrice){
+        this.Name = pName;
+        this.Price = pPrice;
+    }
+}
---
+++ src/java/webapp/products/ProductsServlet.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package webapp.products;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ProductsServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        ArrayList<Product> productList = new ArrayList<Product>();       
+        productList.add(new Product("Coca-Cola", 1500));
+        productList.add(new Product("Pepsi", 1300));
+        productList.add(new Product("Mantequilla", 800));
+        productList.add(new Product("Creso Crema", 700));
+        productList.add(new Product("Jugo de Naranja", 1700));
+
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your pae here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ProductsServlet</title>");
+            out.println("<script src=\"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js\" integrity=\"sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3\" crossorigin=\"anonymous\"></script>");
+            out.println("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js\" integrity=\"sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz\" crossorigin=\"anonymous\"></script>");
+            out.println("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT\" crossorigin=\"anonymous\">");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<div class=\"container\" style=\"margin-top:5%\">");            
+            out.println("   <h2>Product list</h2>");
+            out.println("   <div class=\"list-group\">");            
+            
+            for(Product prod : productList){
+                out.println("       <a href=\"#\" class=\"list-group-item list-group-item-action\">" + prod.Name + " ₡" + prod.Price +"</a>");
+            }                                  
+                        
+            out.println("   <div class=\"list-group\">");    
+            out.println("   <hr/>");    
+            out.println("   <a href=\"index.html\">Go back</a>");    
+            out.println("</div>");            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /*
+    <div class="list-group">    
+        <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+        <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+        <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+        <a class="list-group-item list-group-item-action disabled">A disabled link item</a>
+      </div>
+    
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
